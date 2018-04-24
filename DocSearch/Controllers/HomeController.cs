7ad977c596a80a391118b86be2d1ddef8030824e
Steve Griffith <stgriffi@microsoft.com>
2018-04-24 17:06:42 -0400

    Added new upload page and linked to home page

--- DocSearch/Controllers/HomeController.cs
+++ DocSearch/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +17,63 @@ namespace DocSearch.Controllers
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+
+        public ActionResult Upload()
+        {
+            ViewBag.Title = "Upload Page";
+            ViewBag.Status = "Add a file";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase[] uploadedFiles)
+        {
+
+            if (uploadedFiles != null)
+            {
+                try
+                {
+                    List<string> filesUploaded = new List<string>();
+                    foreach (HttpPostedFileBase file in uploadedFiles)
+                    {
+                        UploadBlob(file);
+                        filesUploaded.Add(file.FileName);
+                    }
+                    ViewBag.filesUploaded = filesUploaded;
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Status = ex.Message;
+                }
+            }
+            else
+            {
+                ViewBag.Status = "No files selected";
+            }
+
+            return View("Upload");
+        }
+
+        private CloudBlobContainer GetCloudBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageAccountConnectionString"]);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("documents");
+            container.CreateIfNotExists();
+
+            return container;
+        }
+
+
+        public void UploadBlob(HttpPostedFileBase file)
+        {
+
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference(file.FileName);
+            blob.UploadFromStream(file.InputStream);
+
         }
     }
 }
